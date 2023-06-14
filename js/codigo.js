bdec987a35a07e398a1583badb490ee10530cf9f
Marcosdalvia <136546879+Marcosdalvia@users.noreply.github.com>
2023-06-14 00:11:16 -0300

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,20 @@
+document.getElementById('menu').addEventListener('click', function () {
+
+    document.getElementById('navega').classList.toggle('mostrar');
+
+
+
+
+})
+
+lightbox.option({
+    'wrapAround': true,
+    'albumLabel': "Imagen %1 de %2",
+    'imageFadeDuration': 1000,
+
+
+
+})
+
+
+
